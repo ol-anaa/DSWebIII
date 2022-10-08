0eba9908c40cab915bd3394efed15c8a58db7437
Ana Beatriz <105469302+ol-anaa@users.noreply.github.com>
2022-10-07 21:20:02 -0300

    a

---
+++ src/components/Carometro/Carometro.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './Carometro.css';
+//import Card from 'react-bootstrap/Card';
+import Main from '../template/Main';
+
+const title = "Carometro";
+
+const urlAPI = "http://localhost:5255/api/carometro";
+const urlAPICurso = "http://localhost:5255/api/curso";
+const urlAPIAluno = "http://localhost:5255/api/aluno";
+
+
+export default class Carometro extends Component {
+
+}
