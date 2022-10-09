e23d7443737cdeac7496c098fbae666abd045ddf
eliseu gili <gili.eliseup@gmail.com>
2022-10-08 21:59:46 -0300

    c

--- escola/src/components/Carometro/Carometro.js
+++ escola/src/components/Carometro/Carometro.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './Carometro.css';
-import Card from 'react-bootstrap/Card';
+//import Card from 'react-bootstrap/Card';
 //import 'bootstrap'
 import Main from '../template/Main';
 
